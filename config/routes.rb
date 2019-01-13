bbf1db511c669f6fd3836690ae6373224b0b01da
ymachida <yasnori1118@gmail.com>
2019-01-13 12:26:53 +0000

    Implement basic login

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get '/help',   to: 'static_pages#help'
   get '/about',  to: 'static_pages#about'
   get '/signup',  to: 'users#new'
-  post '/signup', to: 'users#create'
+  get '/login',   to: 'sessions#new'
+  post '/login',  to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :users
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,6 +8,9 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
   include ApplicationHelper
-
-  # Add more helper methods to be used by all tests here...
+  
+  # テストユーザーがログイン中の場合にtrueを返す
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
 end
