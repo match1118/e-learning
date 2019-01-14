a4f173afd07fddb4c94191f77ace0193fae2a6b9
ymachida <yasnori1118@gmail.com>
2019-01-14 12:17:10 +0000

    User edit and index

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,6 +12,10 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
   
+  def current_user?(user)
+    user == current_user
+  end
+  
    # 現在ログイン中のユーザーを返す (いる場合),記憶トークンcookieに対応するユーザーを返す
   def current_user
     if (user_id = session[:user_id])
@@ -42,6 +46,17 @@ module SessionsHelper
     forget(current_user)
     session.delete(:user_id)
     @curernt_user = nil
+  end
+  
+  # 記憶したURL (もしくはデフォルト値) にリダイレクト
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # アクセスしようとしたURLを覚えておく
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
   
   
